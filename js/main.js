e96d3c8ad21e4a7e620b97e131f56fdc0d7ffbe4
huyenplt <huyen.phamlt19@gmail.com>
2023-03-01 19:22:54 +0700

    ver3

--- js/main.js
+++ js/main.js
@@ -51,12 +51,41 @@ $(document).ready(function () {
     ],
     responsiveClass: false,
   });
-
 });
 
-function submitBookingFrom(event) {
+const bookingModal = document.getElementById('bookingModal')
+bookingModal.addEventListener('show.bs.modal', event => {
+    $('#bookingModal p.noti.success')[0].innerHTML = '';
+    $('#bookingForm #time')[0].value = '';
+    $('#bookingForm #bookingName')[0].value = '';
+    $('#bookingForm #bookingPhone')[0].value = '';
+    $('#bookingForm #bookingEmail')[0].value = '';
+    $('#bookingForm #bookingNote')[0].value = '';
+  })
+
+  const registerModal = document.getElementById('registerModal')
+  registerModal.addEventListener('show.bs.modal', event => {
+    $('#registerModal p.noti.success')[0].innerHTML = '';
+    $('#registerForm #registerNname')[0].value = '';
+    $('#registerForm #registerPhone')[0].value = '';
+    $('#registerForm #registerEmail')[0].value = '';
+    $('#registerForm #license')[0].value = '';
+    $('#registerForm #registerNote')[0].value = '';
+  })
+
+  const buyModal = document.getElementById('buyModal')
+  buyModal.addEventListener('show.bs.modal', event => {
+     $('#buyForm #buyName')[0].value = ''
+      $('#buyForm #buyPhone')[0].value = ''
+      $('#buyForm #buyEmail')[0].value = ''
+      $('#buyForm #buyNote')[0].value = ''
+        $('#buyModal p.noti.success')[0].innerHTML = '';
+  })
+
+const formUrl = 'https://script.google.com/macros/s/AKfycbwTRT8nxJQl21jdpVappqizpGKoiApwV43l4Xsa8PDJRAyubDhjsxXBjp64tVuvqMJW7A/exec';
+
+function submitBookingForm(event) {
   event.preventDefault();
-  console.log('sddd')
 
   var place = $('#bookingForm #place')[0].value;
   var contentSelected = $('#bookingForm .form-check-input:checked');
@@ -74,30 +103,123 @@ function submitBookingFrom(event) {
   var note = $('#bookingForm #bookingNote')[0].value;
   var inputTime = new Date(Date.now());
 
-  var url = 'https://script.google.com/macros/s/AKfycbwTRT8nxJQl21jdpVappqizpGKoiApwV43l4Xsa8PDJRAyubDhjsxXBjp64tVuvqMJW7A/exec';
-
   var data = {
-    name: name,
-    phone: phone,
-    email: email, 
-    content: content,
-    place: place,
-    time: time,
-    note: note,
-    inputTime: inputTime
+    "Form Type": "Đặt lịch hẹn",
+    "Customer Name": name,
+    "Customer Phone": phone,
+    "Customer Email": email, 
+    "Consult Content": content,
+    "Consult Place": place,
+    "Booking Time": time,
+    "Customer Note": note,
+    "Register Time": inputTime
   };
 
   $.ajax({
-    url: url,
+    url: formUrl,
     method: "GET",
     dataType: "json",
     data: data,
     success: function (response, request) {
-      console.log('dsdsd')
+      $('#bookingForm p.noti.success')[0].innerHTML = 'Bạn đã đăng ký thành công! Chúng tôi sẽ liên hệ lại trong thời gian sớm nhất';
+    }
+  });
+}
+
+function submitBuyForm(event) {
+  event.preventDefault();
+
+  var name = $('#buyForm #buyName')[0].value;
+  var phone = $('#buyForm #buyPhone')[0].value;
+  var email = $('#buyForm #buyEmail')[0].value;
+  var note = $('#buyForm #buyNote')[0].value;
+  var inputTime = new Date(Date.now());
+
+  var data = {
+    "Form Type": "Đăng ký mua xe",
+    "Customer Name": name,
+    "Customer Phone": phone,
+    "Customer Email": email, 
+    "Customer Note": note,
+    "Register Time": inputTime
+  };
+
+  $.ajax({
+    url: formUrl,
+    method: "GET",
+    dataType: "json",
+    data: data,
+    success: function (response, request) {
+      $('#buyForm p.noti.success')[0].innerHTML = 'Bạn đã đăng ký thành công! Chúng tôi sẽ liên hệ lại trong thời gian sớm nhất';
     }
   });
 
-  console.log('contentStr', place, content, time, name, phone, email, note)
+}
+
+function submitRegisterForm(event) {
+  event.preventDefault();
+  console.log('sddd');
+
+  var name = $('#registerForm #registerNname')[0].value;
+  var phone = $('#registerForm #registerPhone')[0].value;
+  var email = $('#registerForm #registerEmail')[0].value;
+  var note = $('#registerForm #registerNote')[0].value;
+  var license = $('#registerForm #license')[0].value;
+  var inputTime = new Date(Date.now());
+
+  var data = {
+    "Form Type": "Đăng ký lái thử",
+    "Customer Name": name,
+    "Customer Phone": phone,
+    "Customer Email": email,
+    "Customer's License Status": license,
+    "Customer Note": note,
+    "Register Time": inputTime
+  };
+
+  $.ajax({
+    url: formUrl,
+    method: "GET",
+    dataType: "json",
+    data: data,
+    success: function (response, request) {
+      console.log('dsdsd');
+      $('#registerForm p.noti.success')[0].innerHTML = 'Bạn đã đăng ký thành công! Chúng tôi sẽ liên hệ lại trong thời gian sớm nhất';
+    }
+  });
+
+}
+
+function submitContactForm(event) {
+  event.preventDefault();
+
+  var name = $('#contactForm #name')[0].value;
+  var phone = $('#contactForm #phone')[0].value;
+  var address = $('#contactForm #address')[0].value;
+  var car = $('#contactForm #car-type')[0].value;
+  var payment = $('#contactForm #check-payment input:checked').value;
+  var inputTime = new Date(Date.now());
+
+  var data = {
+    "Form Type": "Liên hệ",
+    "Customer Name": name,
+    "Customer Phone": phone,
+    "Customer Address": address,
+    "Interested Car": car,
+    "Payment Type": payment,
+    "Register Time": inputTime
+  };
+
+  $.ajax({
+    url: formUrl,
+    method: "GET",
+    dataType: "json",
+    data: data,
+    success: function (response, request) {
+      $('#contactForm p.noti.success')[0].innerHTML = 'Bạn đã đăng ký thành công! Chúng tôi sẽ liên hệ lại trong thời gian sớm nhất';
+    }
+  });
+
 }
 
 function toggleSubmenu(e) {
